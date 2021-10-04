1c116e724586e42fbbcf8a004ec7213c3d57289c
GeneralTaoChikn <chrisdiasanta@gmail.com>
2021-10-03 21:56:05 -0400

    changed something in user Experiences

--- HTTP Requests/src/components/survey/UserExperiences.vue
+++ HTTP Requests/src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences()">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -19,12 +19,36 @@
 
 <script>
 import SurveyResult from './SurveyResult.vue';
+import axios from 'axios';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
   },
+  data () {
+    return{
+      results: []
+    }
+  },
+
+  methods: {
+    loadExperiences(){
+      axios.get('https://vue-http-demo-2ce63-default-rtdb.firebaseio.com/surveys.json').then((response) =>{
+        const results =[];
+        for (const id in response){
+          console.log( id +
+          response[id].name +
+          response[id].rating);
+
+          results.push({ id: id, 
+          name: response[id].name,
+           rating: response[id].rating})
+        }
+      this.results = results;
+      });
+    }
+
+  }
 };
 </script>
 
